826b4ae949070549967c3b1b98d41260e5cacc49
Austin <austin@Austins-MacBook-Pro.local>
2015-05-04 00:11:12 -0700

    Rewrote codename.py
    now dynamically imports lists from wordlists folder and creates a menu
    to choose from.

---
+++ codename.OLD.py
@@ -0,0 +1,135 @@
+#!/usr/bin/python -tt
+# Copyright Austin Ellis
+
+# Uses word lists to generate random project "code names" like
+# "Project Flying Squirrel"
+
+import sys
+import os
+import argparse
+import random
+from os.path import isfile, join
+
+
+def file_to_dict(filename):
+    """
+    Takes in a filename including path, opens the file,
+    and returns a dict
+    """
+    f = open(filename, 'rU')
+    wordlist_dict = {}
+    i = 0
+    for line in f:
+        word = line.title()
+        if word not in wordlist_dict.values():
+            wordlist_dict[i] = word
+            i += 1
+    # for key, word in wordlist.iteritems():
+    #    print key, word
+    return wordlist_dict
+
+#wordtype = "animal"
+
+
+def get_word(wordtype, num=1):
+    if wordtype == 'adjective':
+        pass
+    if wordtype == 'animal':
+        pass
+    if wordtype == 'greekgod':
+        pass
+    if wordtype == 'customfile':
+        pass
+
+
+def get_random(wordlist_dict):
+    word = random.choice(wordlist_dict.values())
+    return word.rstrip('\n')
+
+
+def get_animal():
+    filename = 'wordlists/animals.txt'
+    wordlist_dict = file_to_dict(filename)
+    animal = get_random(wordlist_dict)
+    return animal.rstrip('\n')
+
+
+def get_greekgod():
+    filename = 'wordlists/greekgods.txt'
+    wordlist_dict = file_to_dict(filename)
+    greekgod = get_random(wordlist_dict)
+    return greekgod.rstrip('\n')
+
+def get_egyptiangods():
+    filename = 'wordlists/egyptiangods.txt'
+    wordlist_dict = file_to_dict(filename)
+    egyptiangod = get_random(wordlist_dict)
+    return egyptiangod.rstrip('\n')
+
+def get_adjective():
+    filename = 'wordlists/adjectives.txt'
+    wordlist_dict = file_to_dict(filename)
+    adjective = get_random(wordlist_dict)
+    return adjective.rstrip('\n')
+
+
+def print_wordlist(wordlist):
+    for key, word in wordlist.iteritems():
+        print key, word
+
+
+def display_project(random_word, random_adj=False):
+    new_string = []
+    if random_adj != False:
+        new_string.append(random_adj)
+        for item in random_word:
+            new_string.append(item)
+    else:
+        for item in random_word:
+            new_string.append(item)
+
+    join_str = ' '.join(new_string)
+    project_name = 'Project' + ' ' + join_str
+    with open('results.txt', 'a') as results_file:
+        results_file.write(project_name + "\n")
+    return project_name
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--file', help='needs a filename')
+    parser.add_argument('-a', '--animals',
+                        help='Use animals wordlist', action='store_true')
+    parser.add_argument('-gg', '--greekgods',
+                        help='Use Greek gods wordlist', action='store_true')
+    parser.add_argument('-adj', '--adjectives',
+                        help='include adjectives', action='store_true')
+    parser.add_argument('-eg', '--egyptiangods',
+                        help='Use Egyption gods wordlist', action='store_true')
+    args = parser.parse_args()
+    random_word = []
+    random_adj = False
+    if args.file:
+        wordlist_dict = file_to_dict(args.file)
+        random_word.append(get_random(wordlist_dict))
+
+    if args.animals:
+        random_word.append(get_animal())
+
+    if args.greekgods:
+        random_word.append(get_greekgod())
+
+    if args.egyptiangods:
+        random_word.append(get_egyptiangods())
+
+    if args.adjectives:
+        random_adj = get_adjective()
+
+    #project_name_output = 'Project ' + ' ' + random_word
+    # print project_name_output
+    project_name = display_project(random_word, random_adj)
+    print project_name
+
+
+if __name__ == '__main__':
+    main()
--- codename.py
+++ codename.py
@@ -6,8 +6,21 @@
 
 import sys
 import os
-import argparse
+from os import listdir
 import random
+from os.path import isfile, join
+
+
+def load_menu():
+    path_dict = {}
+    path = "/www/Sites/project_melodic_rhino/wordlists"
+    path_list = [f for f in listdir(path) if isfile(join(path, f))]
+    i = 0
+    for item in path_list:
+        if not item.startswith('.'):
+            path_dict[i] = item
+            i += 1
+    return path_dict
 
 
 def file_to_dict(filename):
@@ -27,44 +40,18 @@ def file_to_dict(filename):
     #    print key, word
     return wordlist_dict
 
-#wordtype = "animal"
-
-
-def get_word(wordtype, num=1):
-    if wordtype == 'adjective':
-        pass
-    if wordtype == 'animal':
-        pass
-    if wordtype == 'greekgod':
-        pass
-    if wordtype == 'customfile':
-        pass
-
 
 def get_random(wordlist_dict):
     word = random.choice(wordlist_dict.values())
     return word.rstrip('\n')
 
 
-def get_animal():
-    filename = 'wordlists/animals.txt'
+def get_word(list):
+    filename = 'wordlists/' + list
     wordlist_dict = file_to_dict(filename)
-    animal = get_random(wordlist_dict)
-    return animal.rstrip('\n')
-
-
-def get_greakgod():
-    filename = 'wordlists/greekgods.txt'
-    wordlist_dict = file_to_dict(filename)
-    greekgod = get_random(wordlist_dict)
-    return greekgod.rstrip('\n')
-
-
-def get_adjective():
-    filename = 'wordlists/adjectives.txt'
-    wordlist_dict = file_to_dict(filename)
-    adjective = get_random(wordlist_dict)
-    return adjective.rstrip('\n')
+    word = get_random(wordlist_dict)
+    #print filename
+    return word.rstrip('\n')
 
 
 def print_wordlist(wordlist):
@@ -72,14 +59,14 @@ def print_wordlist(wordlist):
         print key, word
 
 
-def display_project(random_word, random_adj=False):
+def display_project(random_word_list, adjective=''):
     new_string = []
-    if random_adj != False:
-        new_string.append(random_adj)
-        for item in random_word:
+    if adjective != '':
+        new_string.append(adjective)
+        for item in random_word_list:
             new_string.append(item)
     else:
-        for item in random_word:
+        for item in random_word_list:
             new_string.append(item)
 
     join_str = ' '.join(new_string)
@@ -90,39 +77,33 @@ def display_project(random_word, random_adj=False):
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--file', help='needs a filename')
-    parser.add_argument('-a', '--animals',
-                        help='Use animals wordlist', action='store_true')
-    parser.add_argument('-gg', '--greekgods',
-                        help='Use Greek gods wordlist', action='store_true')
-    parser.add_argument('-adj', '--adjectives',
-                        help='include adjectives', action='store_true')
-    parser.add_argument('-eg', '--egyptiangods',
-                        help='Use Egyption gods wordlist', action='store_true')
-    args = parser.parse_args()
-    random_word = []
-    random_adj = False
-    if args.file:
-        wordlist_dict = file_to_dict(args.file)
-        random_word.append(get_random(wordlist_dict))
+    menu = load_menu()
+    random_word_list = []
+    print '\n'
+    print 'Available Lists: \n'
+    for i in menu:
+        print i + 1, ': ', menu[i]
+    print '\n'
+    word_count = int(raw_input('How many words would you like in '
+                               'your project name:'))
+    while word_count > 0:
+        choice = int(raw_input('Select a list: '))
+        choice = choice - 1
+        if 'adjective' in menu[choice]:
+            adjective = get_word(menu[choice])
+            #print adjective
+            word_count -= 1
+        else:
+            #print menu[choice]
+            random_word_list.append(get_word(menu[choice]))
+            word_count -= 1
 
-    if args.animals:
-        random_word.append(get_animal())
-
-    if args.greekgods:
-        random_word.append(get_greakgod())
-
-    if args.egyptiangods:
-        random_word.append(get_egyptiangods())
-
-    if args.adjectives:
-        random_adj = get_adjective()
-
-    #project_name_output = 'Project ' + ' ' + random_word
-    # print project_name_output
-    project_name = display_project(random_word, random_adj)
+    project_name = display_project(random_word_list, adjective)
     print project_name
+        #print random_word_list
+
+#    project_name = display_project(random_word, random_adj)
+#    print project_name
 
 
 if __name__ == '__main__':
